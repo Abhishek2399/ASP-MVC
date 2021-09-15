7b5254c9ee247ca07bf7e24b7ee7d699ed11193d
Abhishek Bhovar <abhibhovar@gmail.com>
2021-09-15 16:57:39 +0530

    Modified MVC

--- MVC_Empty/Controllers/CustomerController.cs
+++ MVC_Empty/Controllers/CustomerController.cs
@@ -4,14 +4,33 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
+
+// When we run this directly we will get error as the page wont be found 
+// we need to add it manually in the url : {local host}https://localhost:44329/ + Customer{Controller name}/Display{Action name} 
 namespace MVC_Empty.Controllers
 {
     public class CustomerController : Controller
     {
-        // GET: Customer
-        public ActionResult Index()
+
+        // To Display some thing on screen we need to return String 
+        // Display is the action method 
+        public string Display()
         {
-            return View();
+            return "Welcome to MVC";
         }
+
+        // Returning int values
+        public int Add()
+        {
+            return 100;
+        }
+
+        // Getting values 
+        // Changes in the url : https://localhost:44329/Customer/Square?id=3
+        public int Square(int id = 0)
+        {
+            return id * id;
+        }
+
     }
 }
